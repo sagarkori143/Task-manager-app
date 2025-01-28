e32406774c8c04ce5bee7f022693973c188f59ee
Sagar Kori <sagarkoriup11@gmail.com>
2025-01-28 13:56:35 +0530

    major changes

--- BackEnd/index.js
+++ BackEnd/index.js
@@ -121,7 +121,7 @@ app.post("/login", (req, res, next) => {
       );
 
 
-      return res.status(200).json({ success: true, message: "Successfully logged in", user });
+      return res.status(200).json({ success: true, token: token, message: "Successfully logged in", user });
     });
   })(req, res, next);
 });
