e411bb56d9372dcdb88791122bee2a5ed3acd5bf
Sagar Kori <sagarkoriup11@gmail.com>
2025-01-29 00:19:38 +0530

    cookies accept kr liye

--- BackEnd/index.js
+++ BackEnd/index.js
@@ -117,7 +117,7 @@ app.post("/login", (req, res, next) => {
       return res.status(200).json({
         success: true,
         message: "Successfully logged in",
-        user,
+        user: req.session.user,
       });
     });
   })(req, res, next);
