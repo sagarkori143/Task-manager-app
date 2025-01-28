26e25b052c07339dd3bd396294c912d6f1127791
Sagar Kori <sagarkoriup11@gmail.com>
2025-01-28 11:47:45 +0530

    server

--- BackEnd/index.js
+++ BackEnd/index.js
@@ -33,7 +33,7 @@ const allowedOrigins = [
 app.use(
   cors({
     origin: (origin, callback) => {
-      if (allowedOrigins.includes(origin)) {
+      if (!origin || allowedOrigins.includes(origin)) {
         callback(null, true);
       } else {
         callback(new Error("Not allowed by CORS"));
