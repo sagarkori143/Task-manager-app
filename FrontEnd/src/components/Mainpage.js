640629d2c875d17fb04d34d3c0b4046ed2d459ed
Sagar Kori <sagarkoriup11@gmail.com>
2025-01-28 22:01:36 +0530

    minor changes

--- FrontEnd/src/components/Mainpage.js
+++ FrontEnd/src/components/Mainpage.js
@@ -54,7 +54,7 @@ export default function Mainpage({ toast, signIn, user }) {
           toast.success("Login successfully");
           user = false;
           localStorage.setItem('authToken', result.data.token);
-          console.log("Login successful, token received!");
+          console.log("Login successful, token received!", localStorage.getItem('token'));
           navigate("/Home");
         } else {
           toast.error("Enter the correct details");
